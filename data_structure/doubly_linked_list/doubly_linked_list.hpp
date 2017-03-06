b8dcd6a0a9a0e25311059e0bbb2ad279b6e17e47
pvlbzn <pvlbzn@gmail.com>
2017-03-06 20:14:15 +0100

    Function declarations and factory methods

--- data_structure/doubly_linked_list/doubly_linked_list.hpp
+++ data_structure/doubly_linked_list/doubly_linked_list.hpp
@@ -1,3 +1,8 @@
+#include <cstdlib>
+
+#pragma once
+
+
 
 /**
  * Circular doubly linked list with a sentinel. list_t appears to be between
@@ -9,6 +14,7 @@ struct list_t
     struct node_t* nil;
 };
 
+
 struct node_t
 {
     int data;
@@ -16,6 +22,30 @@ struct node_t
     struct node_t* prev;
 };
 
+
+list_t* list_factory()
+{
+    list_t *list = new list_t();
+    list->nil = NULL;
+    
+    return list;
+}
+
+
+node_t* node_factory()
+{
+    node_t *node = new node_t();
+    node->data = 0;
+    node->next = NULL;
+    node->prev = NULL;
+
+    return node;
+}
+
+
 node_t* search(list_t *head, int key);
 void    insert(list_t *head, node_t *node);
+void    insert(list_t *head, node_t *node, int at);
 void    remove(list_t *head, node_t *node);
+void    swap(list_t *head, node_t *a, node_t *b);
+int     size(list_t *head);
